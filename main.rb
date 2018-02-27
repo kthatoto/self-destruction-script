4ee321e41dc2932ef09e8db5d3dcfdb1189099d5
kthatoto <kthatoto@gmail.com>
2018-02-27 15:43:18 +0900

    virtual file

--- main.rb
+++ main.rb
@@ -1,32 +1,27 @@
-require 'rubygems'; require 'bundler/setup'; require 'curses'; require 'pp';
-Curses.init_screen; Curses.noecho
+require 'rubygems'; require 'bundler/setup'; require 'curses'; require 'pp'
 self_file_name = File.basename(__FILE__)
-File.read(self_file_name).each_line{|line| Curses.addstr(line)}
-move_keys = {
-  h: {y:  0, x: -1}, j: {y:  1, x:  0},
-  k: {y: -1, x:  0}, l: {y:  0, x:  1}}
+file = File.read(self_file_name).each_line.to_a
+Curses.init_screen; Curses.noecho
+file.each{|line|Curses.addstr(line)}
+move_keys = {h: {y: 0, x: -1}, j: {y: 1, x: 0}, k: {y: -1, x: 0}, l: {y: 0, x: 1}}
 mode = nil
 loop do
-  updated = false;
   key = Curses.getch
   current_position = {y: Curses.stdscr.cury, x: Curses.stdscr.curx}
+  if mode == :replace
+    mode = nil
+    file[current_position[:y]][current_position[:x]] = key
+    Curses.clear
+    file.each{|line|Curses.addstr(line)}
+    Curses.setpos(current_position[:y], current_position[:x])
+    next
+  end
   break if key == 'q'
   Curses.setpos(
     current_position[:y] + move_keys[key.to_sym][:y],
     current_position[:x] + move_keys[key.to_sym][:x]
   ) if move_keys[key.to_sym]
-  if key == 'd'
-    if mode == :deleting
-      Curses.deleteln; mode = nil; updated = true
-    else
-      mode = :deleting
-    end
-  end
-  if key == 'r'
-    updated = true
-  end
-
-  if updated
-  end
+  mode = :replace if key == 'r'
 end
 Curses.close_screen
+`echo -n > #{self_file_name}`
